bf3359ff4f6577278b87835e0054044b28eed483
matsuokashuhei <matsuokashuheiii@gmail.com>
2025-02-24 00:04:51 +0900

    Set up SeaORM and create users table

---
+++ backend/migration/src/m20250222_164410_create_users.rs
@@ -0,0 +1,40 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+use crate::{define_created_at, define_updated_at};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(pk_auto(User::Id))
+                    .col(string(User::Name))
+                    .col(string(User::TimeZone))
+                    .col(&mut define_created_at())
+                    .col(&mut define_updated_at())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum User {
+    #[sea_orm(iden = "users")]
+    Table,
+    Id,
+    Name,
+    TimeZone,
+}
---
+++ backend/src/domain/entities/user.rs
@@ -0,0 +1,20 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "users")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub name: String,
+    pub time_zone: String,
+    pub created_at: DateTime,
+    pub updated_at: DateTime,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelatedEntity)]
+pub enum RelatedEntity {}
