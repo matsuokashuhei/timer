a5c6b85ee746e99e059e8f35bedb09b67e5bcdfe
matsuokashuhei <matsuokashuheiii@gmail.com>
2025-02-23 23:15:36 +0900

    Set up axum

---
+++ backend/src/main.rs
@@ -0,0 +1,11 @@
+use axum::{http::StatusCode, routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(|| async { "Hello world!" }))
+        .route("/health", get(|| async { StatusCode::OK }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
